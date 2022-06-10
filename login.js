cb71e37c9c295d64b9aee12385083a2a12eada3c
charanpatchipala <107140784+charanpatchipala@users.noreply.github.com>
2022-06-11 00:28:08 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "charan" && password == "charan@123"){
 alert ("Login successfully");
-window.location = "index.html"; // Redirecting to other page.
+window.location = "home.html"; // Redirecting to other page.
 return false;
 }
 else{
